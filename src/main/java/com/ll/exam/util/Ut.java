cd54478fe1f06e56d8df1b6d226792b6fd75faf2
ddmkim94 <ddmkim94@gmail.com>
2022-07-28 13:08:07 +0900

    list, map to json

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -2,6 +2,10 @@ package com.ll.exam.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
+
+import java.util.List;
+import java.util.Map;
 
 public class Ut {
     public static class json {
@@ -20,11 +24,28 @@ public class Ut {
             }
         }
 
+        //<T>는 해당 메서드가 제네릭 메서드임을 나타내는 거고, T는 리턴타입을 말하는것!
         public static <T> T toObj(String json, Class<T> clazz, T defaultValue) {
             try {
                 return om.readValue(json, clazz);
             } catch (JsonProcessingException e) {
                 return null;
+            }
+        }
+
+        public static String toList(List<ArticleDto> list, String defaultValue) {
+            try {
+                return om.writeValueAsString(list);
+            } catch (JsonProcessingException e) {
+                return "";
+            }
+        }
+
+        public static String toMap(Map<Integer, ArticleDto> map, String defaultValue) {
+            try {
+                return om.writeValueAsString(map);
+            } catch (JsonProcessingException e) {
+                return "";
             }
         }
     }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -3,6 +3,11 @@ import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
@@ -31,5 +36,33 @@ public class AppTest {
 
         ArticleDto articleDtoFromJson = Ut.json.toObj(json, ArticleDto.class, null);
         assertThat(articleDtoOrigin).isEqualTo(articleDtoFromJson);
+    }
+
+    @Test
+    void ObjectMapperTest__listToJson() throws JsonProcessingException {
+        List<ArticleDto> list = new ArrayList<>();
+        list.add(new ArticleDto(1, "제목1", "내용1"));
+        list.add(new ArticleDto(2, "제목2", "내용2"));
+        list.add(new ArticleDto(3, "제목3", "내용3"));
+
+        String json = Ut.json.toList(list, "");
+        assertThat(json).isEqualTo("""
+                [{"id":1,"title":"제목1","body":"내용1"},{"id":2,"title":"제목2","body":"내용2"},{"id":3,"title":"제목3","body":"내용3"}]
+                """.trim());
+        // 자바의 List를 JSON으로 변환하면 JS의 배열과 형식이 같다.
+    }
+
+    @Test
+    void ObjectMapperTest__mapToJson() throws JsonProcessingException {
+        Map<Integer, ArticleDto> map = new HashMap<>();
+        map.put(1, new ArticleDto(1, "제목1", "내용1"));
+        map.put(2, new ArticleDto(2, "제목2", "내용2"));
+        map.put(3, new ArticleDto(3, "제목3", "내용3"));
+
+        String json = Ut.json.toMap(map, "");
+        assertThat(json).isEqualTo("""
+                {"1":{"id":1,"title":"제목1","body":"내용1"},"2":{"id":2,"title":"제목2","body":"내용2"},"3":{"id":3,"title":"제목3","body":"내용3"}}
+                """.trim());
+        // 자바의 MAP을 JSON으로 변환하면 JS의 객체 형식과 같다.
     }
 }
