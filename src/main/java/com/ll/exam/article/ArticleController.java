2fe1c43a5917b73691225c4260820ba11b5deabb
ddmkim94 <ddmkim94@gmail.com>
2022-07-28 15:34:48 +0900

    메타 데이터를 좀 더 편리하게 추가하기 위한 ResultData 클래스 추가

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -1,5 +1,6 @@
 package com.ll.exam.article;
 
+import com.ll.exam.ResultData;
 import com.ll.exam.Rq;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.util.Ut;
@@ -100,8 +101,7 @@ public class ArticleController {
     public void getArticles(Rq rq) {
         List<ArticleDto> articleDtoList = articleService.findAll();
 
-        Map<String, Object> resultData = Ut.mapOf("resultCode", "S-1", "msg", "성공", "data", articleDtoList);
-
+        ResultData<List<ArticleDto>> resultData = new ResultData<>("S-1", "성공", articleDtoList);
         rq.json(resultData);
     }
 }
